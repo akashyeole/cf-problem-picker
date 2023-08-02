f3c7e822e839b1d47f6874dd19d5d4839b93ebe8
Yeole Akash Ravindra (IFINS IT SBI) <AkashRavindra.Yeole@infineon.com>
2023-08-02 17:14:55 +0530

    Added dummy menu block

--- contentScript.js
+++ contentScript.js
@@ -2,11 +2,19 @@
     const randomPickButtonExists = document.getElementsByClassName("random-pick-button")[0];
     if(!randomPickButtonExists){
         const menuList = document.getElementsByClassName("menu-list main-menu-list")[0];
+        
+        const featureContainer = document.createElement("li");
+        featureContainer.className = "feature-container";
 
         const randomPickButton = document.createElement("img");
         randomPickButton.src = chrome.runtime.getURL("assets/shuffle_button.png");
         randomPickButton.className = "random-pick-button";
-
-        menuList.appendChild(randomPickButton);
+        
+        const featureMenu = document.createElement("div");
+        featureMenu.className = "feature-menu";
+        
+        featureContainer.appendChild(randomPickButton);
+        featureContainer.appendChild(featureMenu);
+        menuList.appendChild(featureContainer);
     }
 })();
